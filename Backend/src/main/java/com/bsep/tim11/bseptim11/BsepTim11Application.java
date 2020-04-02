3c2f3b5fa2962cb8b83fb581332e11312bfafef0
Milan Obrenovic <betwo@Milans-MacBook-Pro.local>
2020-04-02 13:32:08 +0200

    Created a new project

---
+++ Backend/src/main/java/com/bsep/tim11/bseptim11/BsepTim11Application.java
@@ -0,0 +1,13 @@
+package com.bsep.tim11.bseptim11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsepTim11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsepTim11Application.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/bsep/tim11/bseptim11/BsepTim11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsep.tim11.bseptim11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsepTim11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
